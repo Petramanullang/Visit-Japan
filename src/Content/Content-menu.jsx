712e5e4170aaec68044e6b0777d119f0b586f99f
Petramanullang <petra.mainacc@gmail.com>
2023-12-23 20:11:35 +0700

    fixing bug deploy

--- src/Content/Content-menu.jsx
+++ src/Content/Content-menu.jsx
@@ -6,22 +6,22 @@ const ContentMenu = () => {
     <div className="content-menu">
       <div className="cards">
         <div className="card">
-          <img src="./src/assets/img/card-1.jpeg" alt="" />
+          <img src="/card-1.jpeg" alt="" />
           <span className="card-text">OSAKA</span>
           <span className="card-text">大阪 </span>
         </div>
         <div className="card">
-          <img src="./src/assets/img/card-2.jpeg" alt="" />
+          <img src="/card-2.jpeg" alt="" />
           <span className="card-text">OKINAWA</span>
           <span className="card-text">沖縄</span>
         </div>
         <div className="card">
-          <img src="./src/assets/img/card-3.webp" alt="" />
+          <img src="/card-3.webp" alt="" />
           <span className="card-text">KYOTO</span>
           <span className="card-text">京都</span>
         </div>
         <div className="card">
-          <img src="./src/assets/img/card-4.jpeg" alt="" />
+          <img src="/card-4.jpeg" alt="" />
           <span className="card-text">SHIBUYA</span>
           <span className="card-text">渋谷</span>
         </div>
